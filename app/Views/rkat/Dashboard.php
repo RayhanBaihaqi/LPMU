9c5bed8c9aecf216089cfff9ed8c3ae785f4f40f
RayhanBaihaqi <baihaqir78@gmail.com>
2021-07-13 16:52:12 +0700

    dashboar rkat

--- app/Views/rkat/Dashboard.php
+++ app/Views/rkat/Dashboard.php
@@ -1,7 +1,8 @@
 <!DOCTYPE html>
 <html lang="en">
+    <head>
         <meta charset="utf-8">
-        <title>Kanun - Law Firm Website Template</title>
+        <title>Dashboard RKAT</title>
         <meta content="width=device-width, initial-scale=1.0" name="viewport">
         <meta content="Law Firm Website Template" name="keywords">
         <meta content="Law Firm Website Template" name="description">
@@ -14,15 +15,17 @@
         
         <!-- CSS Libraries -->
         <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" rel="stylesheet">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
         <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
         <link href="lib/animate/animate.min.css" rel="stylesheet">
         <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+        
 
         <!-- Template Stylesheet -->
         <link rel="stylesheet" href="http://localhost:8080/css/header.css">
     </head>
 
-    <body>
+    <body onload="startTime()">
         <div class="wrapper">
             <!-- Top Bar Start -->
             <div class="top-bar">
@@ -31,39 +34,53 @@
                         <div class="col-sm-2">
                             <div class="logo">
                                 <a href="index.html">
-                                    <img src="/img/logo_upj.png" alt="Logo">
+                                    <img src="/img/logo-upj.png" alt="Logo">
                                 </a>
                             </div>
                         </div>
-                        <div class="col-sm-8">
+                        <div class="col-sm-5">
+                            <div class="logo">
+                            <h1 class="pertama">Lembaga</h1>
+                            <h1 class="kedua">Penjaminan Mutu</h1>
+                            <h1 class="ketiga">Universitas</h1>
+                            </div>
+                        </div>
+                        <div class="col-sm-1">
                             <div class="top-bar-right">
                                 <div class="text">
-                                    <h2>8:00 - 9:00</h2>
+                                    <h2>RKAT</h2>
                                 </div>
                             </div>
                         </div>
                         <div class="col-sm-2">
-                                <div class="social">
-                                    <a href=""><i class="fab fa-twitter"></i></a>
-                                    <a href=""><i class="fab fa-facebook-f"></i></a>
-                                    <a href=""><i class="fab fa-linkedin-in"></i></a>
-                                    <a href=""><i class="fab fa-instagram"></i></a>
+                            <div class="top-bar-right">
+                                <div class="text">
+                                    <h2><div id="txt"></div></h2>
                                 </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-2">
+                            <div class="social">
+                                <a href=""><i class="fab fa-twitter"></i></a>
+                                <a href=""><i class="fab fa-facebook-f"></i></a>
+                                <a href=""><i class="fab fa-linkedin-in"></i></a>
+                                <a href=""><i class="fab fa-instagram"></i></a>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
+        </div>
             <!-- Top Bar End -->
-
+        <div class="container">
             <!-- Nav Bar Start -->
             <div class="nav-bar">
-                <div class="container-fluid">
                     <nav class="navbar navbar-expand-lg bg-dark navbar-dark">
+                        <a href=""><i class="fas fa-long-arrow-alt-left"></i></a>
                         <a href="#" class="navbar-brand">MENU</a>
                         <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
                             <span class="navbar-toggler-icon"></span>
                         </button>
-
                         <div class="collapse navbar-collapse justify-content-between" id="navbarCollapse">
                             <div class="navbar-nav mr-auto">
                                 <a href="index.html" class="nav-item nav-link active">Home</a>
@@ -81,16 +98,77 @@
                                 <a href="contact.html" class="nav-item nav-link">Contact</a>
                             </div>
                             <div class="ml-auto">
-                                <a class="btn" href="https://htmlcodex.com/law-firm-website-template">Get Appointment</a>
+                                <a href="#" class="btn">LOGOUT</a>
                             </div>
                         </div>
                     </nav>
-                </div>
             </div>
+                <br>
             <!-- Nav Bar End -->
+            <div class="container-fluid">
+            <p>Selamat Datang di halaman RKAT </p>
+            <!-- ====================================================================================================================== -->
+                <div class="row">
+                    <div class="col-lg-3 col-6">
+                    <!-- small box -->
+                        <div class="small-box bg-info">
+                            <div class="inner">
+                                <h3>150</h3>
+                                <p>Dosen</p>
+                            </div>
+                            <div class="icon">
+                                <i class="fas fa-user"></i>
+                            </div>
+                            <a href="#" class="small-box-footer">Lihat detail <i class="fas fa-arrow-circle-right"></i></a>
+                        </div>
+                    </div>
+                    <!-- ./col -->
+                    <div class="col-lg-3 col-6">
+                    <!-- small box -->
+                        <div class="small-box bg-success">
+                            <div class="inner">
+                                <h3>53<sup style="font-size: 20px">%</sup></h3>
+                                <p>Tenaga Pendidik</p>
+                            </div>
+                            <div class="icon">
+                            <i class="fas fa-user"></i>
+                            </div>
+                            <a href="#" class="small-box-footer">Lihat detail <i class="fas fa-arrow-circle-right"></i></a>
+                        </div>
+                    </div>
+                    <!-- ./col -->
+                    <div class="col-lg-3 col-6">
+                    <!-- small box -->
+                        <div class="small-box bg-warning">
+                            <div class="inner">
+                                <h3>44</h3>
+                                <p>Program Studi</p>
+                            </div>
+                            <div class="icon">
+                                <i class="fas fa-graduation-cap"></i>
+                            </div>
+                            <a href="#" class="small-box-footer">Lihat detail <i class="fas fa-arrow-circle-right"></i></a>
+                        </div>
+                    </div>
+                    <!-- ./col -->
+                    <div class="col-lg-3 col-6">
+                    <!-- small box -->
+                        <div class="small-box bg-danger">
+                            <div class="inner">
+                                <h3>65</h3>
+                                <p>Unit Universitas</p>
+                            </div>
+                            <div class="icon">
+                                <i class="fas fa-university"></i>
+                            </div>
+                            <a href="#" class="small-box-footer">Lihat detail <i class="fas fa-arrow-circle-right"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>  
+            <!-- ====================================================================================================================== --> 
             
-            
-            
+        </div>    
 
         <!-- JavaScript Libraries -->
         <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
@@ -98,8 +176,22 @@
         <script src="lib/easing/easing.min.js"></script>
         <script src="lib/owlcarousel/owl.carousel.min.js"></script>
         <script src="lib/isotope/isotope.pkgd.min.js"></script>
-
-        <!-- Template Javascript -->
-        <script src="js/main.js"></script>
+        <script>
+			function startTime() {
+			  var today = new Date();
+			  var h = today.getHours();
+			  var m = today.getMinutes();
+			  var s = today.getSeconds();
+			  m = checkTime(m);
+			  s = checkTime(s);
+			  document.getElementById('txt').innerHTML =
+			  h + ":" + m + ":" + s;
+			  var t = setTimeout(startTime, 500);
+			}
+			function checkTime(i) {
+			  if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+			  return i;
+			}
+		</script>
     </body>
 </html>
