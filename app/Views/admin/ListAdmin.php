d68fb84603f90f4935cd301619a56844a5a4a3ae
alexndrmgw <biggomario555@gmail.com>
2021-08-05 07:45:09 +0700

    update lagi

--- app/Views/admin/ListAdmin.php
+++ app/Views/admin/ListAdmin.php
@@ -104,19 +104,17 @@
 
 						<!-- Nav Item - User Information -->
 						<li class="nav-item dropdown no-arrow">
-							<a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
-								data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+							<a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
 								<span class="mr-2 d-none d-lg-inline text-gray-600 small">
 									<?php
-                                            $nama_prodi = session('nama_prodi');
-                                            echo "$nama_prodi"
-                                        ?>
+									$nama_prodi = session('nama_prodi');
+									echo "$nama_prodi"
+									?>
 								</span>
 								<img class="img-profile rounded-circle" src="/img/inf-logo.jpg">
 							</a>
 							<!-- Dropdown - User Information -->
-							<div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
-								aria-labelledby="userDropdown">
+							<div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
 								<a class="dropdown-item" href="#">
 									<i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
 									Profile
@@ -164,22 +162,18 @@
 									</thead>
 									<tbody>
 										<?php $i = 1; ?>
-										<?php foreach($users as $user) : ?>
-										<tr>
-											<td scope="row"><?= $i++; ?></td>
-											<td><?= $user['username'];?></td>
-											<td><?= $user['passwoard'];?></td>
-											<td><?= $user['nama_prodi'];?></td>
-											<td><?= $user['level'];?></td>
-											<td>
-												<a href="#editEmployeeModal" class="edit" data-toggle="modal"><i
-														class="material-icons" data-toggle="tooltip"
-														title="Edit">&#xE254;</i></a>
-												<a href="#deleteEmployeeModal" class="delete" data-toggle="modal"><i
-														class="material-icons" data-toggle="tooltip"
-														title="Delete">&#xE872;</i></a>
-											</td>
-										</tr>
+										<?php foreach ($users as $user) : ?>
+											<tr>
+												<td scope="row"><?= $i++; ?></td>
+												<td><?= $user['username']; ?></td>
+												<td><?= $user['passwoard']; ?></td>
+												<td><?= $user['nama_prodi']; ?></td>
+												<td><?= $user['level']; ?></td>
+												<td>
+													<a href="#editEmployeeModal" class="edit" data-toggle="modal"><i class="material-icons" data-toggle="tooltip" title="Edit">&#xE254;</i></a>
+													<a href="#deleteEmployeeModal" class="delete" data-toggle="modal"><i class="material-icons" data-toggle="tooltip" title="Delete">&#xE872;</i></a>
+												</td>
+											</tr>
 										<?php endforeach; ?>
 									</tbody>
 								</table>
