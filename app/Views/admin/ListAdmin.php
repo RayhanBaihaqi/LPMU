67f2507e1ad014c81eac1e6ef91dcb35357f3389
RayhanBaihaqi <baihaqir78@gmail.com>
2021-08-08 07:05:10 +0700

    admin

--- app/Views/admin/ListAdmin.php
+++ app/Views/admin/ListAdmin.php
@@ -9,7 +9,7 @@
 	<meta name="description" content="">
 	<meta name="author" content="">
 
-	<title>Admin</title>
+	<title>List data user</title>
 
 	<!-- Custom fonts for this template-->
 	<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" rel="stylesheet">
