2b8403aa550423a7cc661129c4978a2ef0b452d1
Thefi <realth99@gmail.com>
2021-11-24 11:06:09 +0700

    update link dinamis kpi ubah password

--- app/Views/kpi/ubah_pwd.php
+++ app/Views/kpi/ubah_pwd.php
@@ -9,7 +9,7 @@
     <meta content="Law Firm Website Template" name="description">
 
     <!-- Favicon -->
-    <link rel="shortcut icon" type="image/png" href="/favicon.ico" />
+    <link rel="shortcut icon" type="image/png" href="<?php echo base_url(); ?>/public/favicon.ico" />
 
     <!-- Google Font -->
     <link href="https://fonts.googleapis.com/css2?family=EB+Garamond:ital,wght@1,600;1,700;1,800&family=Roboto:wght@400;500&display=swap" rel="stylesheet">
@@ -18,14 +18,14 @@
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
-    <link href="lib/animate/animate.min.css" rel="stylesheet">
-    <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+    <!-- <link href="lib/animate/animate.min.css" rel="stylesheet">
+    <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet"> -->
 
 
     <!-- Template Stylesheet -->
-    <link rel="stylesheet" href="http://localhost:8080/css/header.css">
-    <link rel="stylesheet" href="http://localhost:8080/css/style2.css">
-    <link href="http://localhost:8080/css/dataTables.bootstrap4.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>/public/css/header.css">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>/public/css/style2.css">
+    <link href="<?php echo base_url(); ?>/public/css/dataTables.bootstrap4.min.css" rel="stylesheet">
 
 
 </head>
@@ -38,8 +38,8 @@
                 <div class="row">
                     <div class="col-sm-2">
                         <div class="logo">
-                            <a href="index.html">
-                                <img src="/img/logo-upj.png" alt="Logo">
+                            <a href="<?php echo site_url(); ?>">
+                                <img src="<?php echo base_url(); ?>/public/img/logo-upj.png" alt="Logo">
                             </a>
                         </div>
                     </div>
@@ -84,7 +84,7 @@
         <!-- Nav Bar Start -->
         <div class="nav-bar">
             <nav class="navbar navbar-expand-lg bg-dark navbar-dark">
-                <a href="/backend/kpi"><i class="fas fa-long-arrow-alt-left"></i></a>
+                <a href="<?php echo site_url(); ?>backend/kpi"><i class="fas fa-long-arrow-alt-left"></i></a>
                 <a href="#" class="navbar-brand">MENU</a>
                 <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
                     <span class="navbar-toggler-icon"></span>
@@ -96,7 +96,7 @@
                             <div class="dropdown">
                                 <a class="dropdown-toggle" href="#" role="button" data-toggle="dropdown">
                                     <span class="user-icon">
-                                        <img src="/img/inf-logo.jpg" alt="">
+                                        <img src="<?php echo base_url(); ?>/public/img/inf-logo.jpg" alt="">
                                     </span>
                                     <span class="user-name">
                                         <?php
@@ -185,9 +185,9 @@
     <!-- JavaScript Libraries -->
     <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js"></script>
-    <script src="lib/easing/easing.min.js"></script>
+    <!-- <script src="lib/easing/easing.min.js"></script>
     <script src="lib/owlcarousel/owl.carousel.min.js"></script>
-    <script src="lib/isotope/isotope.pkgd.min.js"></script>
+    <script src="lib/isotope/isotope.pkgd.min.js"></script> -->
     <script>
         function startTime() {
             var today = new Date();
@@ -210,15 +210,15 @@
     </script>
 
     <!-- Bootstrap core JavaScript-->
-    <script src="http://localhost:8080/js/jquery.min.js"></script>
-    <script src="http://localhost:8080/js/bootstrap.bundle.min.js"></script>
+    <script src="<?php echo base_url(); ?>/public/js/jquery.min.js"></script>
+    <script src="<?php echo base_url(); ?>/public/js/bootstrap.bundle.min.js"></script>
 
     <!-- Page level plugins -->
-    <script src="http://localhost:8080/js/jquery.dataTables.min.js"></script>
-    <script src="http://localhost:8080/js/dataTables.bootstrap4.min.js"></script>
+    <script src="<?php echo base_url(); ?>/public/js/jquery.dataTables.min.js"></script>
+    <script src="<?php echo base_url(); ?>/public/js/dataTables.bootstrap4.min.js"></script>
 
     <!-- Page level custom scripts -->
-    <script src="http://localhost:8080/js/datatables-demo.js"></script>
+    <script src="<?php echo base_url(); ?>/public/js/datatables-demo.js"></script>
 
     <script>
         $(window).load(function() {
