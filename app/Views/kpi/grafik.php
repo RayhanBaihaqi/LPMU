ebb1e5e84c31ce1f86b9b64957d9a0ba42c96885
alexndrmgw <biggomario555@gmail.com>
2021-12-18 09:40:00 +0700

    update kpi

--- app/Views/kpi/grafik.php
+++ app/Views/kpi/grafik.php
@@ -11,7 +11,7 @@
     <!-- Theme style -->
     <link rel="stylesheet" href="<?php echo base_url(); ?>/public/dist/css/adminlte.min.css">
     <meta charset="utf-8">
-    <title>Dashboard KPI</title>
+    <title>Kesimpulan</title>
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
     <meta content="Law Firm Website Template" name="keywords">
     <meta content="Law Firm Website Template" name="description">
